be6dc5896ab89beb2295b0a6d500b842fe354a8b
FLH0202 <FLH0202@163.com>
2020-03-13 14:37:58 +0800

    lal

---
+++ 3.13exercise2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cv::Mat firstMat = imread("E:\\PIC\\112.jpg");
+	Mat GrayMat;
+	Mat all_result;
+	Mat local_result;
+	cvtColor(firstMat, GrayMat, CV_BGR2GRAY);
+
+	//全局二值化
+	threshold(GrayMat, all_result, 100, 255, THRESH_BINARY);
+
+	//区域自适应二值化
+	adaptiveThreshold(GrayMat, local_result, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 15, 10);
+
+	imshow("src1", all_result);
+	imshow("src2", local_result);
+	waitKey(0);
+
+}
